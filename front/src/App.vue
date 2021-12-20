fb5a380b9360c1fbb1ad03f0e16294de1ed6405d
sreyngitdoeurm <sreyngit.doeurm@student.passerellesnumeriques.org>
2021-12-20 08:49:50 +0700

    #8 fix some code

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,47 +1,18 @@
 <template>
   <v-app>
-    <v-navigation-drawer
-      app
-      v-model="showNavigation"
-      absolute
-      bottom
-      temporary
-      width="23%"
-    >
-      <menu-item></menu-item>
-    </v-navigation-drawer>
-    <v-app-bar app height="45px">
-      <v-app-bar-nav-icon
-        @click.prevent="showNavigation = !showNavigation"
-      ></v-app-bar-nav-icon>
-      <v-img max-height="50" max-width="250" src=""></v-img>
-      <v-spacer></v-spacer>
-      <v-avatar size="36">
-        <img
-          src="https://i.pinimg.com/originals/a6/58/32/a65832155622ac173337874f02b218fb.png"
-          alt=""
-        />
-      </v-avatar>
-    </v-app-bar>
+    <navigation-bar></navigation-bar>
     <v-main>
-      <v-container fluid ma-0 pa-0 fill-height>
-        <router-view></router-view>
-      </v-container>
+      <router-view></router-view>
     </v-main>
-    <v-footer app> </v-footer>
+    <!-- <v-footer app> </v-footer> -->
   </v-app>
 </template>
 
 <script>
-import Menu from "./components/nav/Menu.vue";
+import Nav from "./components/nav/Navigation.vue";
 export default {
   components: {
-    "menu-item": Menu,
-  },
-  data() {
-    return {
-      showNavigation: false,
-    };
+    "navigation-bar": Nav,
   },
 };
 </script>
---
+++ front/src/components/nav/Navigation.vue
@@ -0,0 +1,62 @@
+<template>
+  <nav>
+    <v-navigation-drawer app v-model="showNavigation" absolute left temporary>
+      <v-list nav dense>
+        <v-list-item :to="{ path: '/myprofile' }">
+          <v-list-item-icon>
+            <v-icon>mdi-account</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>My Profile</v-list-item-title>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-icon>
+            <v-icon>mdi-calendar-today</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>Events</v-list-item-title>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-icon>
+            <v-icon>mdi-account-search</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>Explore Alumni</v-list-item-title>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-icon>
+            <v-icon>mdi-account-multiple</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>Manage Users</v-list-item-title>
+        </v-list-item>
+        <v-divider></v-divider>
+        <v-list-item>
+          <v-list-item-icon>
+            <v-icon>mdi-export</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>Log out</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar app height="45px">
+      <v-app-bar-nav-icon
+        @click.prevent="showNavigation = !showNavigation"
+      ></v-app-bar-nav-icon>
+      <v-img max-height="50" max-width="250" src=""></v-img>
+      <v-spacer></v-spacer>
+      <v-avatar size="36">
+        <img
+          src="https://i.pinimg.com/originals/a6/58/32/a65832155622ac173337874f02b218fb.png"
+          alt=""
+        />
+      </v-avatar>
+    </v-app-bar>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showNavigation: false,
+    };
+  },
+};
+</script>
