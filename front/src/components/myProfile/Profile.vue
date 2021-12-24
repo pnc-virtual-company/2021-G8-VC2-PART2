ab5daef39f46d161d526e12da22c9496d1782e6e
Lyhuoy In <lyhuoy.in@student.passerellesnumeriques.org>
2021-12-24 20:31:17 +0700

    #35 completed view and edit image dialog on frontend

--- front/src/components/myProfile/Profile.vue
+++ front/src/components/myProfile/Profile.vue
@@ -9,20 +9,69 @@
           />
         </v-avatar>
         <div class="img mr-3">
-          <v-btn
-            color="white"
-            fab
-            x-small
-            height="25"
-            width="25"
-            dark
-            elevation="1"
-          >
-            <v-icon color="black">mdi-camera</v-icon>
-          </v-btn>
+          <div class="text-center">
+            <v-dialog v-model="dialog" width="500">
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                  color="white"
+                  fab
+                  x-small
+                  height="25"
+                  width="25"
+                  dark
+                  elevation="1"
+                  v-bind="attrs"
+                  v-on="on"
+                >
+                  <v-icon color="black">mdi-camera</v-icon>
+                </v-btn>
+              </template>
+
+              <v-card>
+                <v-card-title>
+                  <span class="text-h6">Edit Image</span>
+                </v-card-title>
+                <v-divider></v-divider>
+                <v-card-text>
+                  <v-img
+                    class="center"
+                    max-width="350"
+                    :src="image"
+                  >
+                  </v-img>
+                </v-card-text>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <div text class="image-upload mb-3">
+                    <input type="file" @change="fileChange">
+                  </div>
+                  <v-btn
+                    class="mb-3"
+                    small
+                    depressed
+                    color="primary"
+                    text
+                    @click="dialog = false"
+                  >
+                    Cancel
+                  </v-btn>
+                  <v-btn
+                    small
+                    depressed
+                    color="blue"
+                    class="white--text mb-3"
+                    @click="dialog = false"
+                  >
+                    Change
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+          </div>
+          <!--  -->
         </div>
         <div class="d-flex">
-          <h3 class="mt-2">{{alumniData.name}}</h3>
+          <h3 class="mt-2">{{ alumniData.name }}</h3>
           <v-icon>mdi-gender-female</v-icon>
         </div>
       </v-col>
@@ -38,22 +87,22 @@
           <div class="mr-3">
             <v-icon color="#00A3FF">mdi-clipboard-text</v-icon>
           </div>
-          <v-list-item-title>{{alumniData.batch}}</v-list-item-title>
+          <v-list-item-title>{{ alumniData.batch }}</v-list-item-title>
         </v-list-item>
         <v-list-item class="ma-0 pa-0">
           <v-icon class="mr-3" color="#00A3FF">mdi-school</v-icon>
-          <v-list-item-title>{{alumniData.skill}}</v-list-item-title>
+          <v-list-item-title>{{ alumniData.skill }}</v-list-item-title>
         </v-list-item>
       </v-col>
       <v-spacer></v-spacer>
       <v-col class="ma-0">
         <v-list-item class="ma-0 pa-0">
           <v-icon class="mr-3" color="#00A3FF">mdi-email</v-icon>
-          <v-list-item-title>{{alumniData.email}}</v-list-item-title>
+          <v-list-item-title>{{ alumniData.email }}</v-list-item-title>
         </v-list-item>
         <v-list-item class="ma-0 pa-0">
           <v-icon class="mr-3" color="#00A3FF">mdi-phone-in-talk</v-icon>
-          <v-list-item-title>{{alumniData.phone}}</v-list-item-title>
+          <v-list-item-title>{{ alumniData.phone }}</v-list-item-title>
         </v-list-item>
       </v-col>
     </v-row>
@@ -62,7 +111,19 @@
 
 <script>
 export default {
-  props:['alumniData'],
+  props: ["alumniData"],
+  data() {
+    return {
+      dialog: true,
+      image: "https://www.pngall.com/wp-content/uploads/12/Avatar-Profile-PNG-Free-Image.png"
+    };
+  },
+  methods: {
+    fileChange(e) {
+      let file = e.target.files[0];
+      this.image = URL.createObjectURL(file)
+    }
+  },
 };
 </script>
 
@@ -76,5 +137,14 @@ export default {
 .edit-info {
   display: flex;
   justify-content: flex-end;
+}
+.center {
+  display: block;
+  margin: auto;
+  width: 50%;
+  margin-top: 10px;
+}
+.v-card-title {
+  background: rgb(168, 96, 96);
 }
 </style>
